9c262ce0d03a76f308e1cf76480fb4d6fc95d45d
Masa Zeradjanin <masa.zeradjanin.17@singimail.rs>
2021-09-08 21:37:04 +0200

    Init project

---
+++ 02-resources/mojabaza.sql
@@ -0,0 +1,181 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server version:               10.1.37-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for aplikacija
+DROP DATABASE IF EXISTS `aplikacija`;
+CREATE DATABASE IF NOT EXISTS `aplikacija` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `aplikacija`;
+
+-- Dumping structure for table aplikacija.administrator
+DROP TABLE IF EXISTS `administrator`;
+CREATE TABLE IF NOT EXISTS `administrator` (
+  `administrator_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password_hash` int(255) NOT NULL,
+  `is_active` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`administrator_id`),
+  UNIQUE KEY `uq_administrator_username` (`username`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table aplikacija.cart
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE IF NOT EXISTS `cart` (
+  `cart_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`cart_id`),
+  KEY `fk_cart_user_id` (`user_id`),
+  CONSTRAINT `fk_cart_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table aplikacija.cart_furniture
+DROP TABLE IF EXISTS `cart_furniture`;
+CREATE TABLE IF NOT EXISTS `cart_furniture` (
+  `cart_furniture` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `quantity` int(10) unsigned NOT NULL,
+  `cart_id` int(10) unsigned NOT NULL,
+  `furniture_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`cart_furniture`),
+  UNIQUE KEY `uq_cart_furniture_cart_id` (`cart_id`),
+  UNIQUE KEY `uq_cart_furniture_furniture_id` (`furniture_id`),
+  CONSTRAINT `fk_cart_furniture_cart_id` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_cart_furniture_furniture_id` FOREIGN KEY (`furniture_id`) REFERENCES `furniture` (`furniture_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table aplikacija.category
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `parent__category_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `uq_category_name` (`name`),
+  KEY `fk_category_parent__category_id` (`parent__category_id`),
+  CONSTRAINT `fk_category_parent__category_id` FOREIGN KEY (`parent__category_id`) REFERENCES `category` (`category_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table aplikacija.furniture
+DROP TABLE IF EXISTS `furniture`;
+CREATE TABLE IF NOT EXISTS `furniture` (
+  `furniture_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `title` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descr_cons` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dimensions` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `color` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `material` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_available` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `category_id` int(10) unsigned NOT NULL,
+  `location_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`furniture_id`),
+  UNIQUE KEY `uq_furnituretitle` (`title`),
+  KEY `fk_furniture_category_id` (`category_id`),
+  KEY `fk_furniture_location_id` (`location_id`),
+  CONSTRAINT `fk_furniture_category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_furniture_location_id` FOREIGN KEY (`location_id`) REFERENCES `location` (`location_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table aplikacija.furniture_price
+DROP TABLE IF EXISTS `furniture_price`;
+CREATE TABLE IF NOT EXISTS `furniture_price` (
+  `furniture_price_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `price` decimal(10,2) unsigned NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `furniture_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`furniture_price_id`),
+  KEY `fk_furniture_price_furniture_id` (`furniture_id`),
+  CONSTRAINT `fk_furniture_price_furniture_id` FOREIGN KEY (`furniture_id`) REFERENCES `furniture` (`furniture_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table aplikacija.location
+DROP TABLE IF EXISTS `location`;
+CREATE TABLE IF NOT EXISTS `location` (
+  `location_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `address` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `latitude` decimal(10,8) NOT NULL,
+  `longitude` decimal(10,8) NOT NULL,
+  `company_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`location_id`),
+  KEY `fk_location_company_id` (`company_id`),
+  CONSTRAINT `fk_location_company_id` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table aplikacija.order
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE IF NOT EXISTS `order` (
+  `order_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `status` enum('pending','rejected','accepted','completed') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'pending',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `cart_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`order_id`),
+  UNIQUE KEY `uq_order_cart_id` (`cart_id`),
+  CONSTRAINT `fk_order_cart_id` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table aplikacija.photo
+DROP TABLE IF EXISTS `photo`;
+CREATE TABLE IF NOT EXISTS `photo` (
+  `photo_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `image_path` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `furniture_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`photo_id`),
+  UNIQUE KEY `uq_photo_image_path` (`image_path`),
+  KEY `fk_photo_furniture_id` (`furniture_id`),
+  CONSTRAINT `fk_photo_furniture_id` FOREIGN KEY (`furniture_id`) REFERENCES `furniture` (`furniture_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table aplikacija.user
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `email` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password_hash` int(255) NOT NULL,
+  `password_reset_code` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `forename` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `surname` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(24) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `postal_address` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_active` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `uq_user_email` (`email`) USING BTREE,
+  UNIQUE KEY `uq_user_phone` (`phone`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
---
+++ README.md
@@ -0,0 +1,10 @@
+# Predmet Praktikum Internet i Veb Tehnologije
+
+## Sadrzaj spremista
+ Ovde se nalazi projekat za predmet praktikum internet i veb tehnologije, tema projekta je Veb aplikacija za radnju za prodaju nameštaja
+## Struktura spremista
+
+* U direktorijumu [01-documentation](./01-documentation) se nalazi dokumentacija ovog projekta.
+* U direktorijumu [02-resources](./02-resources) se nalaze dodatni materijali za ovaj projekat.
+* U direktorijumu [03-back-end](./03-back-end) se nalazi kod projekta za API.
+* U direktorijumu [04-front-end](./04-front-end) se nalazi front end deo aplikacije.
