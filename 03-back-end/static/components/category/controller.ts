c975b28975c8b8885d4a6f431fe33ac0a53b90c8
Masa Zeradjanin <masa.zeradjanin.17@singimail.rs>
2021-09-09 13:20:53 +0200

    Create a mechanism to get a single cat(mock)

--- 03-back-end/static/components/category/controller.ts
+++ 03-back-end/static/components/category/controller.ts
@@ -8,11 +8,32 @@ class CategoryController{
 private categoryService: CategoryService;
 
 constructor(categoryService: CategoryService){
-this.categoryService = categoryService; 
-} 
+this.categoryService = categoryService;
+ } 
 async getAll(req: Request, res: Response, next: NextFunction){
     const categories = await this.categoryService.getAll();
     res.send(categories);
 }
+async getById(req: Request, res: Response, next: NextFunction){
+
+    const id : string = req.params.id;
+    const categoryId: number = +id;
+    
+    if(categoryId <= 0){
+        res.sendStatus(400);
+        return;
+    }
+
+    const category : CategoryModel | null = await this.categoryService.getById(categoryId);
+    if (category === null){
+        res.sendStatus(404);
+        return;
+    }
+    
+    
+    res.send(category);
+
+}
+
 }
 export default CategoryController;
